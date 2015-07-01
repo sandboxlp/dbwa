b3a5e94647daa5ceeeb9faf8aeb905da1484f734
jemx <jemx@shmy.li>
2015-07-01 21:06:07 +0200

    making it fancy andnot  working

--- content/item.class.php
+++ content/item.class.php
@@ -18,14 +18,16 @@ class item {
         $this->db = $db;
     }
 
-    public function categories_by_pos(){
+    public function categories_by_pos()
+    {
         //$res = mysqli_query($db, "select * from `categories` order by `pos`;");
 
-        $res = $this->db->query("SELECT * from `categories` ORDER BY `pos`;");
+        $res = $this->db->query("SELECT * FROM `categories` ORDER BY `pos` ASC;");
         $result_array = array();
 
-        while($dsatz = mysqli_fetch_assoc($res))
+        while ($dsatz = mysqli_fetch_assoc($res)) {
             array_push($result_array, $dsatz);
+        }
 
         return $result_array;
     }
@@ -33,11 +35,12 @@ class item {
     public function news_by_nid() {
         //$res = mysqli_query($db, "select * from `news` order by `n_id`;");
 
-        $res = $this->db->query("select * from `news` order by `n_id`;");
+        $res = $this->db->query("SELECT * FROM `news` ORDER BY `n_id` ASC;");
         $result_array = array();
 
-        while($dsatz = mysqli_fetch_assoc($res))
+        while($dsatz = mysqli_fetch_assoc($res)) {
             array_push($result_array, $dsatz);
+        }
 
         return $result_array;
     }
