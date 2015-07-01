7f3d161d1a48f907d4c3e8dcbf8caa2802076364
jemx <jemx@shmy.li>
2015-07-01 21:16:47 +0200

    make it working

--- content/db.php
+++ content/db.php
@@ -1,3 +1,4 @@
 <?php
     $db = new mysqli("127.0.0.1", "phpstorm", "123456", "dbwa");
+    $db->query("SET NAMES 'utf8'");
 ?>
--- index2.php
+++ index2.php
@@ -3,31 +3,17 @@
 <?php require("content/top2.php"); ?>
 
     <main>
-        <!-- <a href="<?php //echo htmlspecialchars($dsatz["url"]) ?>" class="invisible">
-                <div class="square-box">
-                    <div class="square-content">
-                        <div>
-                            <span>
-                                <?php //echo "\n"; //echo $dsatz["bez"]."\n"; ?>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </a> -->
-
         <?php
-        require("content/db.php");
-        header('Content-Type: text/html; charset=utf-8');
-        $i = 0;
-        //print_r($item->categories_by_pos());
         foreach ($item->categories_by_pos() as $dsatz) {
+        echo "test";
             if(!empty($dsatz["bez"])) { ?>
-                <a href="<?php echo $dsatz["url"]; ?>">
+                <a href="<?php echo $dsatz["url"]; ?>" >
                     <div class="square-box">
                         <div class="square-content">
                             <div>
                                 <span>
                                     <?php echo $dsatz["bez"]."\n"; ?>
+                                            Hallo das ist ein Test!
                                 </span>
                             </div>
                         </div>
