41e267419e8218b613545d2d84301613b4ed361c
sandboxlp <accounts@web-neuwirth.at>
2015-07-04 15:01:04 +0200

    Finished index.php Finished news.php  added some functions to content/item.class.php  some changes in style.css  created content/nav_boxes/

---
+++ content/nav_boxes/wishes.php
@@ -0,0 +1,11 @@
+<a href="wishes.php" class="invisible">
+    <div class="square-box-small">
+        <div class="square-content">
+            <div>
+                <span>
+                    besondere<br/>Wünsche
+                </span>
+            </div>
+        </div>
+    </div>
+</a>
