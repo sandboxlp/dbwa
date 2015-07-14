add1280823a41ffcbf7b563c7d3eb0530b4f2db7
sandboxlp <accounts@web-neuwirth.at>
2015-07-14 17:19:28 +0200

    Added events.php

--- content/item.class.php
+++ content/item.class.php
@@ -13,6 +13,11 @@ class item { //(TO GET) ALL ABOUT ITEMS
     /** MECKER VON TOBI:
      *  1. SQL-BEFEHLE WERDEN GROß GESCHRIEBEN!
      *  2. ALLES WAS IN DIE KLASSE REIN KOMMT, WIRD ESCAPED (Du weißt nie wie blöd der User ist)
+     *
+     *  RÜCKMECKER VON SANDY:
+     *  1. WENN ES SINN MACHT, MACH ICHS EH! GIBT MIR ZEIT ZUM LERNEN!
+     *  2. WENN ICH GEWUSSTE HABE, WAS DAS IST, HÄTT ICHS GEMACHT!
+     *  3. WENN DU WAS FÜR MICH KOMPLETT NEUES EINBRINGT, ERKLÄRS BITTE - DANKE!
      */
 
     public function __construct($db){
@@ -50,6 +55,17 @@ class item { //(TO GET) ALL ABOUT ITEMS
     public function news_by_nid() {
 
         $res = $this->db->query("SELECT * FROM `news` ORDER BY `n_id`;");
+
+        $result_array = array();
+        while($dsatz = mysqli_fetch_assoc($res))
+            array_push($result_array, $dsatz);
+
+        return $result_array;
+    }
+
+    public function events_by_eid()
+    {
+        $res = $this->db->query("SELECT * FROM `events` ORDER BY `e_id`;");
 
         $result_array = array();
         while($dsatz = mysqli_fetch_assoc($res))
---
+++ events.php
@@ -0,0 +1,44 @@
+<?php require("content/top.php"); ?>
+
+    <style>
+        .date {
+            width:32%;
+            display: inline-block;
+            margin-right: 2%;
+            font-weight: bold;
+            text-align: right;
+            float:left;
+        }
+
+        .title {
+            display: inline-block;
+            float:right;
+            width: 66%;
+        }
+    </style>
+
+<?php require("content/top2.php"); ?>
+
+    <main>
+        <h1 class="superheadline">EVENTS</h1>
+
+        <?php
+        foreach($item->events_by_eid() as $dsatz)
+        {
+            echo "<p>";
+            $date_arr = explode("-", $dsatz["date"]);
+            $date = $date_arr[2].".".$date_arr[1];
+            echo '<span class="date">';
+            if(isset($dsatz["prefix"]))
+                echo $dsatz["prefix"]. " ";
+            echo $date."</span>";
+            echo '<span class="title">'.$dsatz["title"].'</span></p>';
+        } ?>
+        <div style="width: 100%; float:right;"></div>
+        <br/><br/>
+        <?php include("content/nav_boxes/home.php"); ?>
+        <?php include("content/nav_boxes/wishes.php"); ?>
+
+    </main>
+
+<?php require("content/bottom.php"); ?>
